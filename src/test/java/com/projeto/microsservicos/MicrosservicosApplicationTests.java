f2e960ced4865e83f157851919e855f689617151
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-02-16 20:51:15 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/projeto/microsservicos/MicrosservicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.microsservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
